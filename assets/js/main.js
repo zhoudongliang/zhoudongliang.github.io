9327ef246e8c9bfdd3f36b91a7723f94eaf8238f
如此的完美和无懈可击 <zhoudongliang@me.com>
2022-10-24 16:46:51 +0800

    XFindfile准备上线
    还差截图

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,7 @@
 					'<nav><a class="link depth-0" href="screensaver.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>屏保库</a></nav>' +
 					'<nav><a class="link depth-0" href="videoicon.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>视频封面编辑器</a></nav>' +
 					'<nav><a class="link depth-0" href="topweb.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>置顶浏览器</a></nav>' +
+					'<nav><a class="link depth-0" href="xfindfile.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>XFindFile</a></nav>' +
 					'<nav><a class="link depth-0" href="privacy.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>相关政策</a></nav>' +
 					'<nav><a class="link depth-0" href="about.html" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-0"></span>关于我</a></nav>' +
 				'</div>'
